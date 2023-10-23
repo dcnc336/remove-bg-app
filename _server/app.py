5d4fdfb8b6d7585ed46b46f0f48199f60d96910e
dcnc336 <dcnc336@gmail.com>
2023-10-23 14:10:58 -0400

    update server

--- _server/app.py
+++ _server/app.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, request, jsonify, send_file ,make_response,render_template
 from flask_cors import CORS
 from time import perf_counter
-# from backgroundremover import utilities
-# from backgroundremover.bg import remove
+from backgroundremover import utilities
+from backgroundremover.bg import remove
 
 def remove_image_background(input_path, output_path, debugger=False):
     ''' NOTE: Models and modules will take some extra time to load during your first execution.\n
@@ -14,7 +14,7 @@ def remove_image_background(input_path, output_path, debugger=False):
     # Set default parameters for background remover function.
     default_parameters = {
     'model': 'u2net',
-    'alpha_matting': False,
+    'alpha_matting': True,
     'alpha_matting_foreground_threshold': 240,
     'alpha_matting_background_threshold': 10,
     'alpha_matting_erode_size': 10,
@@ -49,7 +49,15 @@ def remove_image_background(input_path, output_path, debugger=False):
     
     # Remove background from the image and return it as a bytes object.
     t1 = perf_counter()
-    img_removed_bg_bytes = remove(image, model_name=default_parameters['model'],alpha_matting=default_parameters['alpha_matting'], alpha_matting_foreground_threshold=default_parameters['alpha_matting_foreground_threshold'],alpha_matting_background_threshold=default_parameters['alpha_matting_background_threshold'],alpha_matting_erode_structure_size=default_parameters['alpha_matting_erode_size'],alpha_matting_base_size=default_parameters['alpha_matting_base_size'])
+    img_removed_bg_bytes = remove(
+        image, 
+        model_name=default_parameters['model'],
+        alpha_matting=default_parameters['alpha_matting'], 
+        alpha_matting_foreground_threshold=default_parameters['alpha_matting_foreground_threshold'],
+        alpha_matting_background_threshold=default_parameters['alpha_matting_background_threshold'],
+        alpha_matting_erode_structure_size=default_parameters['alpha_matting_erode_size'],
+        alpha_matting_base_size=default_parameters['alpha_matting_base_size']
+    )
     if debugger: print(f"{output_path} done. Took {(perf_counter()-t1):003} seconds.")
 
     # Write bytes object to your output path.
@@ -73,8 +81,9 @@ def upload_file():
     if file:
         print(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        # remove_image_background(f"uploads/{file.filename}", f"outputs/{file.filename}")
-        return make_response(send_file(f"uploads/{file.filename}",download_name=file.filename,as_attachment=True))
+        remove_image_background(f"uploads/{file.filename}", f"outputs/{file.filename}")
+        return make_response(send_file(f"outputs/{file.filename}",download_name=file.filename,as_attachment=True))
+        # return make_response(send_file(f"uploads/{file.filename}",download_name=file.filename,as_attachment=True))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8000', debug=True)
